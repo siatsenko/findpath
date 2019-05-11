9250e51862e969918fb297957f8947a7e5caf38f
siatsenko <siatsenko.luxoft@gmail.com>
2019-05-11 10:40:37 +0300

    InputHandler tests

--- src/main/java/InputHandler.java
+++ src/main/java/InputHandler.java
@@ -5,8 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputHandler {
+    private char statrChar = 'S';
+    private char finishChar = 'X';
 
     private Reader reader;
+
+    public InputHandler() {
+    }
 
     public InputHandler(Reader reader) {
         this.reader = reader;
@@ -33,15 +38,26 @@ public class InputHandler {
     }
 
     boolean checkValidInputList(List<String> list) {
+        boolean startFound = false;
+        boolean finishFound = false;
+
         if ((list == null) || (list.size() == 0) || list.get(0).length() == 0) {
-            return false;
+            throw new RuntimeException("Wrong list size");
         }
         int columnsCount = list.get(0).length();
         for (String s : list) {
             int length = s.length();
             if (length != columnsCount) {
-                return false;
+                throw new RuntimeException("Wrong list size");
             }
+            startFound = startFound || s.contains(String.valueOf(statrChar));
+            finishFound = finishFound || s.contains(String.valueOf(finishChar));
+        }
+        if (!startFound) {
+            throw new RuntimeException("Start marker not found");
+        }
+        if (!finishFound) {
+            throw new RuntimeException("Finish marker not found");
         }
         return true;
     }
--- src/test/java/InputHandlerTest.java
+++ src/test/java/InputHandlerTest.java
@@ -1,6 +1,7 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import java.io.Reader;
 import java.io.StringReader;
 import java.util.Arrays;
 import java.util.List;
@@ -15,38 +16,69 @@ public class InputHandlerTest {
             "......#.#....\n" +
             "........#.X..\n";
 
-    private static final List<String> OUTPUT_LIST = Arrays.asList(
+    private static final List<String> CORRECT_LIST = Arrays.asList(
             ".............",
             "..S...#..#...",
             "......#.#....",
             "........#.X.."
     );
 
-    private static final String INPUT_STRING_WRONG_1 = "" +
-            ".............\n" +
-            "..S...#..#...\n" +
-            "......#.#....\n" +
-            "........#....\n";
+    private static final List<String> WRONG_LIST_1 = Arrays.asList(
+            "............",
+            "..S...#..#...",
+            "......#.#....",
+            "........#.X.."
+    );
 
-    private static final String INPUT_STRING_WRONG_2 = "" +
-            ".............\n" +
-            "......#..#...\n" +
-            "......#.#....\n" +
-            "........#.X..\n";
+    private static final List<String> WRONG_LIST_2 = Arrays.asList(
+            ".............",
+            "......#..#...",
+            "......#.#....",
+            "........#.X.."
+    );
 
-    private static final String INPUT_STRING_WRONG_3 = "" +
-            "............\n" +
-            "..S...#..#...\n" +
-            "......#.#....\n" +
-            "........#.X..\n";
+    private static final List<String> WRONG_LIST_3 = Arrays.asList(
+            ".............",
+            "..S...#..#...",
+            "......#.#....",
+            "........#...."
+    );
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
 
     @Test
     public void getFromReader() {
         List<String> list = new InputHandler(new StringReader(INPUT_STRING_CORRECT)).getFromReader();
-        assertEquals(OUTPUT_LIST,list);
+        assertEquals(CORRECT_LIST,list);
     }
 
     @Test
     public void checkValidInputList() {
+        InputHandler inputHandler = new InputHandler();
+        assertTrue(inputHandler.checkValidInputList(CORRECT_LIST));
     }
+
+    @Test
+    public void checkValidInputListWrong1() {
+        expectedEx.expect(RuntimeException.class);
+        expectedEx.expectMessage("Wrong list size");
+        InputHandler inputHandler = new InputHandler();
+        new InputHandler().checkValidInputList(WRONG_LIST_1);
+    }
+
+    @Test
+    public void checkValidInputListWrong2() {
+        expectedEx.expect(RuntimeException.class);
+        expectedEx.expectMessage("Start marker not found");
+        new InputHandler().checkValidInputList(WRONG_LIST_2);
+    }
+
+    @Test
+    public void checkValidInputListWrong3() {
+        expectedEx.expect(RuntimeException.class);
+        expectedEx.expectMessage("Finish marker not found");
+        new InputHandler().checkValidInputList(WRONG_LIST_3);
+    }
+
 }
